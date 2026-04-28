88e95032562521eb4c79db9cec6854d94ded7cdf
Roman <Roman.Bodnar.ShI.2023@lpnu.ua>
2026-04-29 00:37:11 +0300

    feat: refine Azure network security group configuration by adding outbound rules for Grafana

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -78,7 +78,19 @@ resource "azurerm_network_security_group" "nsg" {
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
-  
+
+  security_rule {
+    name                       = "Prometheus"
+    priority                   = 1004
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "9090"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
 }
 
 resource "azurerm_network_interface" "nic" {
