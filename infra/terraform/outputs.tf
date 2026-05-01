e4db05e18106cefb33ba53718d185ec103f09f82
Roman <Roman.Bodnar.ShI.2023@lpnu.ua>
2026-05-01 15:27:01 +0300

    chore: further optimize Kubernetes deployment configuration by refining resource limits and enhancing performance settings

--- infra/terraform/outputs.tf
+++ infra/terraform/outputs.tf
@@ -13,6 +13,11 @@ output "argocd_url" {
   description = "URL для доступу до Argo CD UI"
 }
 
+output "app_url" {
+  value       = "http://${azurerm_public_ip.pip.ip_address}:30080"
+  description = "URL для доступу до GitOps-застосунку (NodePort)"
+}
+
 output "ssh_private_key" {
   value     = tls_private_key.ssh.private_key_pem
   sensitive = true
